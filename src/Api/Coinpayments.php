f7e83213d510be707337c288a52475a2d045f323
NastyaGos <72504315+NastyaGos@users.noreply.github.com>
2021-02-19 01:31:50 -0800

    Use Paid status to confirm payment

--- src/Api/Coinpayments.php
+++ src/Api/Coinpayments.php
@@ -26,7 +26,6 @@ class Coinpayments
 
     const PAID_EVENT = 'Paid';
     const CANCELLED_EVENT = 'Cancelled';
-    const PENDING_EVENT = 'Pending';
 
     const WEBHOOK_NOTIFICATION_URL = '/PaymentCoinpayments/notification';
     /**
